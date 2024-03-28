f5c4d110a7dc8baf081ea3198fa5e3ba7122f195
rushi2163 <rushikeshjadhav7410135881@gmail.com>
2024-03-28 15:22:04 +0530

    forgot password api with email send

--- backend/routs/user.js
+++ backend/routs/user.js
@@ -2,6 +2,12 @@ const express = require('express');
 const connection = require('../connection');
 const router = express.Router();
 
+// using JWT token
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+// node mailer to send password to the user
+const nodemailer = require('nodemailer');
+
 router.post('/signup',(req,res)=>{
     let user= req.body;
     
@@ -34,16 +40,21 @@ router.post('/login', (req,res)=>{//59.42
     const user = req.body;
     query = "select email, password, role, status from user where email=?";
     connection.query(query,[user.email],(err,result)=>{
+        // console.log(result);
         if(!err){
             if(result.length<=0|| result[0].password!=user.password){
                 return res.status(401).json({message:"Incorrect username or password"});
             }
-            else if(result.status==='false'){
+            else if(result[0].status==='false'){
+                // user statue is used to decide whether user can use it or not
                 return res.status(401).json({message:"Wait for admin approval"});
             }
             else if(result[0].password==user.password){
                 //we are generating jwt token  hear
-                console.log("");
+                const response ={email:result[0].email,role:result[0].role}
+                const accessToken = jwt.sign(response,process.env.ACCESS_TOKEN, {expiresIn:'1h'});
+                //using login information a jwt token will be created and it have expiry
+                res.status(200).json({token:accessToken}) ;
 
             }
             else{
@@ -52,6 +63,48 @@ router.post('/login', (req,res)=>{//59.42
         }
         else{
             return res.status(500).json(err)
+        }
+    })
+})
+
+
+
+var transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:process.env.EMAIL,
+        pass:process.env.PASSWORD
+    }
+})
+
+router.post('/forgotpassword',(req,res)=>{
+    let user= req.body;
+    var query="select email,password from user where email=?";
+    connection.query(query,[user.email],(err,result)=>{
+        if(!err){
+            if(result.length<=0){
+                return res.status(200).json({message:"email sent Sucessfully"});
+            }
+            else{//1.17.14
+                var mailoptions={
+                    from:process.env.EMAIL,
+                    to:result[0].email,
+                    subject:"reset password by cafemanagement",
+                    html:`<p>email:${result[0].email}</p><br><p>password:${result[0].password}</p>`
+                }
+                transporter.sendMail(mailoptions,function(error,info){
+                    if(error){
+                        console.log(error);
+                    }
+                    else{
+                        console.log(info.response);
+                    }
+                })
+                return res.status(200).json({message:"email sent Sucessfully"});
+            }
+        }
+        else{
+            return res.status(500).json(err);
         }
     })
 })
