f6a3084f4fb26b8a7235a75d5f95a9b071982cca
rushi2163 <rushikeshjadhav7410135881@gmail.com>
2024-03-29 00:01:37 +0530

    categoty api is added in which we can add category of the product (add,update,get)

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express= require( 'express');
 
 const connection= require('./connection');
 const userRoute=require('./routs/user');
+const categoryRoute=require('./routs/category');
 
 var cors= require( 'cors')
 const app = express();
@@ -10,6 +11,6 @@ app.use(cors());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use('/user',userRoute);//when you go to user path it will call userRoute file
-
+app.use('/category',categoryRoute)
 
 module. exports= app;
--- backend/table.sql
+++ backend/table.sql
@@ -8,5 +8,20 @@ create table user(
     role varchar(20),
     UNIQUE (email)
 );
+create table category(
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    primary key(id)
+);
+
+create table product(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	categoryId integer NOT NULL,
+	description varchar(255),
+	price integer,
+	status varchar(20),
+	primary key(id)
+)
 
 insert into user(name,contactNumber,email,password ,status,role) values( 'Admin','7410135881','rushikeshjadhav7410135881@gmail.com','admin','true','admin');
