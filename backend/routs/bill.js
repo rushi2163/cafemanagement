e8a4ac06f12112007e63ec8c048a8bb950c281d8
rushi2163 <rushikeshjadhav7410135881@gmail.com>
2024-03-29 16:51:01 +0530

    get a bill api functionality is added

--- backend/routs/bill.js
+++ backend/routs/bill.js
@@ -9,7 +9,7 @@ var uuid= require('uuid');
 var auth= require('../services/authentication') ;
 var fs=require('fs') ;
 
-
+// it generate pdf 
 router.post('/generateReport',auth.authenticateToken,(req,res,next)=>{
     let orderDetails=req.body;
     const generateUUID= uuid.v1();
@@ -56,6 +56,45 @@ router.post('/generateReport',auth.authenticateToken,(req,res,next)=>{
             return  res.status(500).json({err:err,message:"error in main query"});
         }
     })
+})
+
+
+//this method is used to send pdf to user using uuid 
+router.post('/getPdf',auth.authenticateToken,function(req,res){
+    const orderDetails = req.body;
+    const pdfPath = './generated_pdf/'+orderDetails.uuid+'.pdf' ;
+    if(fs.existsSync(pdfPath) ) {
+        res.contentType( "application/pdf" ) ;
+        fs.createReadStream(pdfPath).pipe(res) ;
+    }
+    else{
+        
+       var productDetailsReport = JSON.parse(orderDetails. productDetails );
+       ejs.renderFile(path.join(__dirname,"report.ejs"),{
+        productDetails:productDetailsReport,
+        name:orderDetails.name,
+        email:orderDetails.email,
+        contactNumber:orderDetails.contactNumber,
+        paymentMethod:orderDetails.paymentMethod,
+        totalAmount:orderDetails.totalAmount
+    },(err,result)=>{
+        if(err){
+            return  res.status(500).json({message:"error in ejs.render",err:err});
+        }
+        else{
+            pdf.create(result).toFile('./generated_pdf/'+orderDetails.uuid+".pdf",function(err, data){
+                if(err){
+                    return  res.status(500).json({message:"error in pdf.create",err:err});
+                }
+                else{
+                    res.contentType( "application/pdf" ) ;
+                    fs.createReadStream(pdfPath).pipe(res) ;
+                }
+            })
+        }
+    })
+    }
+    
 })
 
 module.exports=router;
