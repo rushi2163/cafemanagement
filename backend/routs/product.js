6903c1ed705069571ea0e0dcee47b8dbb532c5bb
rushi2163 <rushikeshjadhav7410135881@gmail.com>
2024-03-29 11:31:17 +0530

    in product api update status is added for product availability

--- backend/routs/product.js
+++ backend/routs/product.js
@@ -5,7 +5,7 @@ var auth = require('../services/authentication');
 var checkRole = require( '../services/checkRole');
 
 
-
+// to add product of specific category
 router.post('/add',auth.authenticateToken,checkRole.checkRole,(req,res,next)=>{
     let product=req.body;
     query="insert into product (name,categoryId,description,price,status) values(?,?,?,?,'true')";
@@ -18,7 +18,7 @@ router.post('/add',auth.authenticateToken,checkRole.checkRole,(req,res,next)=>{
         }
     })
 })
-
+// to get all product by joining product and category table
 router.get('/get',auth.authenticateToken,(req,res,next)=>{
     let category=req.body;
     query='select p.id,p.name,p.description,p.price,p.status,c.id as categoryId,c.name as categoryName from product as p inner join category as c where p.categoryId=c.id';
@@ -35,7 +35,7 @@ router.get('/get',auth.authenticateToken,(req,res,next)=>{
 
 
 
-
+// to get product of specific category
 router.get('/getByCategory/:id',auth.authenticateToken,(req,res,next)=>{
     const id =req.params.id;
     query ="select id,name from product where categoryId=? and status= 'true'";    
@@ -49,13 +49,13 @@ router.get('/getByCategory/:id',auth.authenticateToken,(req,res,next)=>{
     })
 })
 
-
+// to get a singlt product 
 router.get('/getByID/:id',auth.authenticateToken,(req,res,next)=>{
     const id =req.params.id;
     query ="select id,name,description,price from product where id=?";    
     connection.query(query,[id],(err,result)=>{
         if(!err){
-            return res.status(200).json(result[0]);
+            return res.status(200).json(result[0]);// result[0]  is used because we want to pass data in json if we remove [0] index it will pass data in array form
         }
         else{
             return  res.status(500).json({message:err});
@@ -64,7 +64,7 @@ router.get('/getByID/:id',auth.authenticateToken,(req,res,next)=>{
 })
 
 
-
+// to update product 
 router.patch('/update',auth.authenticateToken,checkRole.checkRole,(req,res,next)=>{
     let product=req.body;
     query='update product set name=?,categoryId=?,description=?,price=? where id=?';
@@ -81,7 +81,7 @@ router.patch('/update',auth.authenticateToken,checkRole.checkRole,(req,res,next)
     })
 })
 
-
+//to delete a specific product using its id
 router.delete('/delete/:id',auth.authenticateToken,checkRole.checkRole,(req,res,next)=>{
     const id =req.params.id;
     query="delete from product where id=?";
@@ -91,6 +91,23 @@ router.delete('/delete/:id',auth.authenticateToken,checkRole.checkRole,(req,res,
                 return res.status(404).json({message:"Product id dose not found"});
             }
             return res.status(200).json({message:"product deleted Successfully"});
+        }
+        else{
+            return  res.status(500).json({message:err});
+        }
+    })
+})
+
+// to update status of the product ( availablity of the product )
+router.patch('/updateStatus',auth.authenticateToken,checkRole.checkRole,(req,res,next)=>{
+    let product=req.body;
+    query='update product set status=? where id=?';
+    connection.query(query,[product.status,product.id],(err,result)=>{
+        if(!err){
+            if(result.affectedRows==0){
+                return res.status(404).json({message:"Product id dose not found"});
+            }
+            return res.status(200).json({message:"Product status updated Successfully"});
         }
         else{
             return  res.status(500).json({message:err});
