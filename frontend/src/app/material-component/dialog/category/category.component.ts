af01244e025f090937bc3a1b545063b43999c41c
rushi2163 <rushikeshjadhav7410135881@gmail.com>
2024-04-01 00:47:34 +0530

    add category and edit category is implemented

--- frontend/src/app/material-component/dialog/category/category.component.ts
+++ frontend/src/app/material-component/dialog/category/category.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CategoryService } from 'src/app/services/category.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { GlobalConstant } from 'src/app/shared/global-constants';
 
 @Component({
   selector: 'app-category',
@@ -6,10 +11,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
+  onAddCategory = new EventEmitter();
+  onEditCategory = new EventEmitter();
+  categoryForm:any=FormGroup;
+  dialogAction :any ="Add";
+  action: any ="Add";
+  responsemessage:any
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public dialogData:any,
+    private formBuilder:FormBuilder,
+    // private route:Router,
+    private categoryService:CategoryService,
+    private snackbarService:SnackbarService,
+    private dialogRef:MatDialogRef<CategoryComponent>
+    
+  ) { }
+
+  ngOnInit(): void { 
+    this.categoryForm=this.formBuilder.group({
+      name:[null,[Validators.required]],
+    })
+
+
+    if(this.dialogData.action=='Edit' ){
+      this.dialogAction="Edit";
+      this.action="Update"
+      this.categoryForm.patchValue(this.dialogData.data) ;
+    }
+  }
+  handleSubmit(){
+    if(this.dialogData.action=='Edit' ){
+      this.edit();
+
+    }else{
+      
+      this.add()
+    }
+  }
+  edit(){
+    var formData=this.categoryForm.value;
+    var data ={
+      id:this.dialogData.data.id,
+      name:formData.name,
+    }
+    this.categoryService.update(data).subscribe((response:any)=>{
+      this.dialogRef.close() ;
+      this.onEditCategory.emit( ) ;
+      this.responsemessage = response.message;
+      this.snackbarService.openSnackBar( this.responsemessage," success " ) ;
+    },(error)=>{
+      this.dialogRef.close() ;
+      if(error.error?.message){
+        this.responsemessage=error.error?.message;}
+      else{
+        this.responsemessage= GlobalConstant.genericError
+      }
+      this.snackbarService.openSnackBar(this.responsemessage,GlobalConstant.genericError)
+    })
+  }
+  add(){
+    var formData=this.categoryForm.value;
+    var data ={
+      name:formData.name,
+    }
+    this.categoryService.add(data).subscribe((response:any)=>{
+      this.dialogRef.close() ;
+      this.onAddCategory.emit() ;
+      this.responsemessage = response.message;
+      this.snackbarService.openSnackBar( this.responsemessage," success " ) ;
+    },(error)=>{
+      //working 7.14.00
+
+      this.dialogRef.close() ;
+      if(error.error?.message){
+        this.responsemessage=error.error?.message;}
+      else{
+        this.responsemessage= GlobalConstant.genericError
+      }
+      this.snackbarService.openSnackBar(this.responsemessage,GlobalConstant.genericError)
+    })
   }
 
 }
--- frontend/src/app/material-component/manage-category/manage-category.component.ts
+++ frontend/src/app/material-component/manage-category/manage-category.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { CategoryService } from 'src/app/services/category.service';
 import { SnackbarService } from 'src/app/services/snackbar.service';
 import { GlobalConstant } from 'src/app/shared/global-constants';
+import { CategoryComponent } from '../dialog/category/category.component';
 
 @Component({
   selector: 'app-manage-category',
@@ -44,6 +45,36 @@ export class ManageCategoryComponent implements OnInit {
     const filterValue=(event.target as HTMLInputElement).value
     this.dataSource.filter=filterValue.trim().toLowerCase()
   }
-  handleAddAction(){}
-  handleEditAction(value:any){}
+  handleAddAction(){
+    const dialogConfig= new MatDialogConfig();
+    dialogConfig.data={
+      action:'Add'
+    }
+    dialogConfig.width="550px";
+    const dialogRef=this.dialog.open(CategoryComponent,dialogConfig)
+    this.router.events.subscribe(()=>{
+      dialogRef.close();
+    })
+    const sub=dialogRef.componentInstance.onAddCategory.subscribe(()=>{
+      this.tableData()
+    })
+      
+  }
+  handleEditAction(values:any){
+    console.log(values);
+    
+    const dialogConfig= new MatDialogConfig();
+    dialogConfig.data={
+      data:values,
+      action:'Edit'
+    }
+    dialogConfig.width="550px";
+    const dialogRef=this.dialog.open(CategoryComponent,dialogConfig)
+    this.router.events.subscribe(()=>{
+      dialogRef.close();
+    })
+    const sub=dialogRef.componentInstance.onEditCategory.subscribe(()=>{
+      this.tableData()
+    })
+  }
 } 
